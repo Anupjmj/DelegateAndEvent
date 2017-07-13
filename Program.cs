1bed377fb182e097cc1ad5bc21382f97cc00947c
Joseph <anoop.joseph@cgi.com>
2017-07-14 04:54:10 +0530

    Hi
    Signed-off-by: Joseph <anoop.joseph@cgi.com>

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAndEventExample
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            var video = new Video() {Title="Video" };
+            var videoEncoder = new VideoEncorder();
+           
+            MailService mailS = new MailService(videoEncoder);
+            MessageService messageSer = new MessageService(videoEncoder);
+            videoEncoder.VideoEncoder(video);
+            Console.Read();
+            }
+        }
+    public class MailService
+        {
+          public  MailService(VideoEncorder ve)
+            {
+            ve.OnEncoded += OnVideoEncoded;
+            }
+        public void OnVideoEncoded(object source,EventArgs e)
+              {
+              Console.WriteLine("Mail Sent");
+              }
+        }
+    public class MessageService
+        {
+        public MessageService(VideoEncorder ve)
+            {
+            ve.OnEncoded += OnVideoEncoded;
+            }
+        public void OnVideoEncoded(object source, EventArgs e)
+            {
+            Console.WriteLine("Message Sent");
+            }
+        }
+    }
---
+++ VideoEncorder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DelegateAndEventExample
+    {
+   public class VideoEncorder
+        {
+       public delegate void OnVideoEncodedEventHandler(object sender,EventArgs e);
+       public event OnVideoEncodedEventHandler OnEncoded;
+       public void VideoEncoder(Video videoObj)
+           {
+           Console.WriteLine("Video Encoding......");
+           Thread.Sleep(30000);
+           Console.WriteLine("video is encoded...");
+           Console.WriteLine("informing all subscriber classes...");
+           Thread.Sleep(1000);
+           OnVideoEncoded();
+           }
+       protected virtual void OnVideoEncoded()
+           {
+           if(OnEncoded!=null)
+               {
+               OnEncoded(this, EventArgs.Empty);
+               }
+           }
+        }
+   
+    }
